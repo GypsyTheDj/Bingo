384c5339d295c374cb668197e311f27a8fcb57f8
GypsyTheDj <gypsythedj@gmail.com>
2020-12-18 21:51:58 +0300

    Bugs Fixes and Performance Improvements

--- app/src/main/java/com/normannuthu/bingo/MainActivity.kt
+++ app/src/main/java/com/normannuthu/bingo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.normannuthu.bingo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var movesTv: TextView
+    private lateinit var pairsTv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        movesTv = findViewById(R.id.movesTv)
+        pairsTv = findViewById(R.id.pairsTv)
+
+
+        rvBoard.adapter = MemoryBoardAdapter(this,8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
     }
 }
